d8bb3d59eaa0e26c536d66f6d21ebdef14f9d405
Miriam Moser <mirjoy.moser@gmail.com>
2015-06-08 14:30:56 -0600

    refactored locatePiece method

--- bundle.js
+++ bundle.js
@@ -56,71 +56,74 @@
 	  this.canvas  = document.getElementById(canvasId);
 	  this.context = this.canvas.getContext('2d');
 	  this.pieces  = createPieces(this.context);
-	  this.pieces[Math.floor(Math.random()*this.pieces.length)].toggleState();
+	  this.numberOfPlays = 0;
+	  this.timer = 0;
 	};
 
-	Game.prototype.init = function () {
+	Game.prototype.init = function (){
 	  this.bindClickHandler();
 	  this.render();
 	}
 
-	Game.prototype.bindClickHandler = function() {
+	Game.prototype.bindClickHandler = function(){
 	  var game = this;
 	  this.canvas.addEventListener("click", function(e) {
 	    var canvasLeft = game.canvas.offsetLeft;
 	    var canvasTop = game.canvas.offsetTop;
 	    var relLeft = (e.pageX - canvasLeft) - 40;
 	    var relTop = (e.pageY - canvasTop) - 40;
-	    var piece = game.locatePiece(relLeft, relTop).toggleState();
-	    game.findNeighbors();
+	    var piece = game.locatePiece(relLeft, relTop);
+	    debugger;
+	    piece.toggleState();
+	    game.findNeighbors(piece);
 	    game.render();
-
-	    console.log("left ", canvasLeft, " top ", canvasTop)
-	    console.log("pleft ", e.pageX, " ptop ", e.pageY)
-	    console.log("rel left ", relLeft, " rel top ", relTop)
-	    console.log(e);
-	    console.log(piece);
-	    console.log(piece.state);
 	  });
 	}
 
 	Game.prototype.locatePiece = function(x,y){
-	  var foundPiece = null;
-
-	  this.pieces.forEach(function(piece){
-	    if ((piece.centerX - 50 < x && x < piece.centerX + 50) &&
-	        (piece.centerY - 50 < y && y < piece.centerY + 50)) {
-	      foundPiece = piece;
-	    }
-	  });
-
-	  return foundPiece;
+	   return this.pieces.filter(function(piece){
+	   if ((piece.centerX - 50 < x && x < piece.centerX + 50) &&
+	      (piece.centerY - 50 < y && y < piece.centerY + 50)){
+	    return piece;
+	   }
+	  }).pop();
 	}
 
-	Game.prototype.findNeighbors = function(piece){
+	Game.prototype.isWon = function(){
+	  var ended = true
+	    this.pieces.forEach(function(piece){
+	      if (piece.isLit){
+	        ended = false
+	      }
+	    })
+	  return ended
+	}
 
-	debugger;
+	Game.prototype.findNeighbors = function(clickedPiece){
 	  var neighbors = [];
 
 	  this.pieces.forEach(function(piece){
-	    if ((piece.centerX - 100 < x && x < piece.centerX + 100) &&
-	        (piece.centerY - 100 < y && y < piece.centerY + 100)) {
+
+	    if (((clickedPiece.x + 80 === piece.x || clickedPiece.x - 80 === piece.x) && clickedPiece.y === piece.y) ||
+	      ((clickedPiece.y + 80 === piece.y || clickedPiece.y - 80 === piece.y) && clickedPiece.x === piece.x)) {
 	      neighbors.push(piece);
 	    }
 	  });
 
-	  return neighbors.forEach(toggleState);
+	  neighbors.forEach(function(piece){
+	    return piece.toggleState();
+	  });
 	}
 
 	Game.prototype.render = function() {
 	  var game = this;
 	  this.pieces.forEach(function(piece) {
-	     game.renderPiece(piece);
+	    game.renderPiece(piece);
 	  });
 	}
 
 	Game.prototype.renderPiece = function(piece) {
-	  if (piece.state === 'lit') {
+	  if (piece.isLit) {
 	    var fillColor = '#d3d3d3'
 	  }
 	  else {
@@ -137,21 +140,22 @@
 	  this.context.stroke();
 	}
 
-	var Piece = function(coordinates, state){
-	  this.state = state || 'lit';
+	var Piece = function(coordinates){
+	  var canvasOffset = 40;
+	  this.isLit = false;
 	  this.x = coordinates['x'];
 	  this.y = coordinates['y'];
-	  this.centerX = this.x - 40;
-	  this.centerY = this.y - 40;
+	  this.centerX = this.x - canvasOffset;
+	  this.centerY = this.y - canvasOffset;
 	}
 
 
 	Piece.prototype.toggleState = function(){
-	  if (this.state === 'lit'){
-	    return this.state = 'unlit';
+	  if (this.isLit){
+	    return this.isLit = false;
 	  }
 	  else {
-	    return this.state = 'lit';
+	    return this.isLit = true;
 	  }
 	}
 
@@ -167,8 +171,24 @@
 	    }
 	  }
 
+	  setInitialLitPieces(pieces);
+
 	  return pieces;
 	};
+
+	function shuffle(o){
+	    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	    return o;
+	}
+
+	var newGameBoard = [[7,11,12,13,17], [0,1,2,6], [13,17,18,19,23], [15,20,21],
+	                    [7,11,12,13,17, 0,1,2,6], [2,7,8,10,11,13,16,17,18,19,23]];
+
+	var setInitialLitPieces = function(pieces){
+	  shuffle(newGameBoard).pop().forEach(function(pieceIndex){
+	    pieces[pieceIndex].toggleState();
+	  });
+	}
 
 	window.onload = function(){
 	  var game = new Game("gameBoard");
